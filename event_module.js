b6839be91cbbc897b0bd6cf805a4ee61d95fdaa4
mano <manomanogaran008@gmail.com>
2026-02-08 22:46:40 +0530

    this is event module

---
+++ event_module.js
@@ -0,0 +1,12 @@
+
+
+const EventEmitter = require("events");
+const emitter = new EventEmitter();
+
+
+
+emitter.on("greet", (name) => {
+  console.log(`Hello, ${name}!`);
+});
+
+emitter.emit("greet", "Mano");
